b9988b1ad7c8b632467adf49151e9bfef52a7f4d
Mateus Silveira Ribeiro <76142179+mateussr7@users.noreply.github.com>
2021-07-14 00:49:43 -0300

    Create: create the project base

---
+++ TP-Eng-Software/src/main/java/com/eng/software/tp/TPEngSoftware/TpEngSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.eng.software.tp.TPEngSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpEngSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpEngSoftwareApplication.class, args);
+	}
+
+}
---
+++ TP-Eng-Software/src/test/java/com/eng/software/tp/TPEngSoftware/TpEngSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eng.software.tp.TPEngSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpEngSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
